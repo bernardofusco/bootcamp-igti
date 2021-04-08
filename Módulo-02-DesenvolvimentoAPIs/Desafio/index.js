eb189da8efcde3c833deb3ffe46241373da74fce
Bernardo Fusco <blfuscomail@gmail.com>
2021-04-08 17:15:46 -0300

    Desafio Módulo 02 - API de notas de alunos

--- Módulo-02-DesenvolvimentoAPIs/Desafio/index.js
+++ Módulo-02-DesenvolvimentoAPIs/Desafio/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import gradesRouter from './routes/grades.js';
 
 const app = express();
+global.fileName = './files/grades.json';
 
-app.get('/', (_, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
+
+app.use('/grade', gradesRouter);
 
 app.listen(3001, () => {
   console.log('API Started!');
